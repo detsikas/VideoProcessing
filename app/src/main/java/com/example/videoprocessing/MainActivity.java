4f1dcb88600463afa75978dbd58e5ca8fada5de6
peeknpoke <contact@peeknpoke.net>
2020-04-27 14:13:34 +0300

    Load and show video, process button

--- app/src/main/java/com/example/videoprocessing/MainActivity.java
+++ app/src/main/java/com/example/videoprocessing/MainActivity.java
@@ -1,22 +1,75 @@
 package com.example.videoprocessing;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int EXTERNAL_STORAGE_PERMISSION_REQUEST = 0x1;
+    private static final int PICK_FROM_GALLERY = 1;
+    private String [] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    private VideoView videoView;
+    private Button processButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.videoView);
+        processButton = findViewById(R.id.process);
     }
 
-    public void onRecord(View view)
-    {}
-
     public void onLoad(View view)
-    {}
+    {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, PICK_FROM_GALLERY);
+    }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_FROM_GALLERY && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            videoView.setVideoURI(uri);
+            videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    videoView.start();
+                    processButton.setVisibility(View.INVISIBLE);
+                }
+            });
+
+            videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    processButton.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+    }
+
+
+    // Permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean permissionGranted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        if (!permissionGranted )
+            finish();
+    }
 }
