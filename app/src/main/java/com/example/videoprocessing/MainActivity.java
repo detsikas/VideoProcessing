dbc5200a4d3887ca56f4c478ab3ceca12d57861e
peeknpoke <contact@peeknpoke.net>
2020-04-27 13:53:07 +0300

    First commit, buttons only

---
+++ app/src/main/java/com/example/videoprocessing/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.videoprocessing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onRecord(View view)
+    {}
+
+    public void onLoad(View view)
+    {}
+    
+
+}
